c3ffb6b9a3e646ab21f87df9a6202428737bf916
Shaojung <shaojung@gmail.com>
2016-12-02 15:59:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/yvd120204/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.user.yvd120204;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.user.yvd120204.data.Student;
+import com.example.user.yvd120204.data.StudentDAO;
+import com.example.user.yvd120204.data.StudentDAOMemoryImpl;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        StudentDAO dao = new StudentDAOMemoryImpl();
+        dao.add(new Student(1, "Bob", "123", "aa123"));
+        dao.add(new Student(2, "Mary", "345", "bb345"));
+        List<Student> lst = dao.getList();
+        for (Student s : lst)
+        {
+            Log.d("DATA", s.toString());
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/user/yvd120204/data/StudentDAOMemoryImpl.java
@@ -0,0 +1,28 @@
+package com.example.user.yvd120204.data;
+
+import android.support.annotation.ArrayRes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by user on 2016/12/2.
+ */
+
+public class StudentDAOMemoryImpl implements StudentDAO {
+    ArrayList<Student> data;
+    public StudentDAOMemoryImpl()
+    {
+        data = new ArrayList<>();
+    }
+
+    @Override
+    public List<Student> getList() {
+        return data;
+    }
+
+    @Override
+    public void add(Student s) {
+        data.add(s);
+    }
+}
