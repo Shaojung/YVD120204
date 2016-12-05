c65037b3e924e942b40b7ec55fe86525108ecd82
Shaojung <shaojung@gmail.com>
2016-12-05 09:07:49 +0800

    Use ListView

--- app/src/main/java/com/example/user/yvd120204/MainActivity.java
+++ app/src/main/java/com/example/user/yvd120204/MainActivity.java
@@ -3,20 +3,26 @@ package com.example.user.yvd120204;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.user.yvd120204.data.Student;
 import com.example.user.yvd120204.data.StudentDAO;
 import com.example.user.yvd120204.data.StudentDAOMemoryImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> data;
+    ListView lv;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        lv = (ListView) findViewById(R.id.listView);
+        data = new ArrayList<>();
         StudentDAO dao = new StudentDAOMemoryImpl();
         dao.add(new Student(1, "Bob", "123", "aa123"));
         dao.add(new Student(2, "Mary", "345", "bb345"));
@@ -24,7 +30,11 @@ public class MainActivity extends AppCompatActivity {
         for (Student s : lst)
         {
             Log.d("DATA", s.toString());
+            data.add(s.toString());
         }
+        adapter = new ArrayAdapter<String>(MainActivity.this,
+                            android.R.layout.simple_list_item_1, data);
+        lv.setAdapter(adapter);
 
     }
 }
