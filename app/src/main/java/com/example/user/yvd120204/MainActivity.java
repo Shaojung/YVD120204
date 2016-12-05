c7a3630c1e0e7751a2ac46d074fce48a60d2dfb3
Shaojung <shaojung@gmail.com>
2016-12-05 09:39:13 +0800

    Custom Application

--- app/src/main/java/com/example/user/yvd120204/MainActivity.java
+++ app/src/main/java/com/example/user/yvd120204/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.user.yvd120204;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,25 +20,46 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> data;
     ListView lv;
     ArrayAdapter<String> adapter;
+    MyApplication app;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lv = (ListView) findViewById(R.id.listView);
         data = new ArrayList<>();
-        MyApplication app = (MyApplication) getApplication();
+        app = (MyApplication) getApplication();
         app.dao = new StudentDAOMemoryImpl();
         app.dao.add(new Student(1, "Bob", "123", "aa123"));
         app.dao.add(new Student(2, "Mary", "345", "bb345"));
+        adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, data);
+        lv.setAdapter(adapter);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         List<Student> lst = app.dao.getList();
+        data.clear();
         for (Student s : lst)
         {
             Log.d("DATA", s.toString());
             data.add(s.toString());
         }
-        adapter = new ArrayAdapter<String>(MainActivity.this,
-                            android.R.layout.simple_list_item_1, data);
-        lv.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("ADD");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent it = new Intent(MainActivity.this, AddActivity.class);
+        startActivity(it);
+        return super.onOptionsItemSelected(item);
     }
 }
